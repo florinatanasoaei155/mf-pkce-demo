78d810faa5a417fc620d03c5b3d88269a0c40adb
Florin Atănăsoaei <atanasoaeiflorin@gmail.com>
2025-02-10 19:42:33 +0200

    add Docker config

--- my-app/src/oidc-config.ts
+++ my-app/src/oidc-config.ts
@@ -1,16 +1,8 @@
-import { OidcConfiguration, TokenAutomaticRenewMode } from "@axa-fr/react-oidc";
+import { OidcConfiguration } from "@axa-fr/react-oidc";
 
 export const oidcConfiguration: OidcConfiguration = {
   client_id: "local-oidc-client",
   redirect_uri: "http://localhost:5173/authentication/callback",
-  silent_redirect_uri: "http://localhost:5173/authentication/silent-callback",
-  scope: "openid profile email api offline_access",
-  authority: "http://localhost:9000",
-  token_automatic_renew_mode:
-    TokenAutomaticRenewMode.AutomaticOnlyWhenFetchExecuted,
-  refresh_time_before_tokens_expiration_in_second: 60,
-  service_worker_relative_url: "/OidcServiceWorker.js",
-  service_worker_only: false,
-  monitor_session: true,
-  preload_user_info: true,
+  authority: "http://localhost:9000", // Identity Server
+  scope: "openid profile email",
 };
